f76cc5a65063bfe5bf4b4419903fab1e0ab32088
Radoslaw <rtarnowski123@gmail.com>
2018-05-01 13:12:45 +0200

    metoda trapezow

---
+++ metoda-trapezow/src/metoda/trapezow/MetodaApp.java
@@ -0,0 +1,29 @@
+package metoda.trapezow;
+
+public class MetodaApp {
+
+	public static void main(String[] args) {
+		
+		long startTime = System.currentTimeMillis();
+		metodaTrapezow(1, 14, 4000000);
+		long endTime = System.currentTimeMillis();
+		
+		long duration = (endTime - startTime);
+		System.out.println(duration + "ms");
+		
+	}
+	
+	public static double f(double x) {
+		return Math.pow(x, 3) + (2*x - 1); 
+	}
+	public static double metodaTrapezow(double xp,double xk,double n) {
+		double dx;
+		double s = 0;
+		dx = (xk-xp)/n;
+		for(int i = 1; i < n; i++) {
+			s += f(xp+i*dx);
+		}
+		return s = (s + (f(xp)+f(xk))/2)*dx;
+	}
+
+}
