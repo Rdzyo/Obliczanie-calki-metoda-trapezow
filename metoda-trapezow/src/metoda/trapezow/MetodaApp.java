7252feba157117320661089ecfac1bc1b0f70479
Radoslaw <rtarnowski123@gmail.com>
2018-05-08 13:47:05 +0200

    update for giving n parameter in a shell script

--- metoda-trapezow/src/metoda/trapezow/MetodaApp.java
+++ metoda-trapezow/src/metoda/trapezow/MetodaApp.java
@@ -3,9 +3,12 @@ package metoda.trapezow;
 public class MetodaApp {
 
 	public static void main(String[] args) {
-		
+		double n = 0;
+		for(String arg : args) {
+			n = Integer.parseInt(arg);
+		}
 		long startTime = System.currentTimeMillis();
-		metodaTrapezow(1, 14, 4000000);
+		metodaTrapezow(1, 14, n);
 		long endTime = System.currentTimeMillis();
 		
 		long duration = (endTime - startTime);
@@ -14,7 +17,7 @@ public class MetodaApp {
 	}
 	
 	public static double f(double x) {
-		return Math.pow(x, 3) + (2*x - 1); 
+		return Math.pow(x, 3) + 2*x - 1; 
 	}
 	public static double metodaTrapezow(double xp,double xk,double n) {
 		double dx;
